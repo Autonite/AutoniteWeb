3e643554107298d12a68877370b8049ae65e443c
Javier Lizca <lesomar7130@gmail.com>
2020-11-19 07:36:47 -0500

    Terminado proveedor validado

--- js/RProveedor.js
+++ js/RProveedor.js
@@ -4,7 +4,7 @@ function validar(){
     Dempresa = document.getElementById("Dempresa").value;
     Telefono = document.getElementById("Telefono").value;
     Nempresa = document.getElementById("Nempresa").value;
-    expresion = /\d/g;
+    expresion1 = /\d/g;
     
     
     
@@ -22,6 +22,11 @@ function validar(){
     return false;
 }
 
+if(!expresion1.test(Documento)){
+    Swal.fire("El documento solo puede contener numeros")
+    return false
+}
+
 if(Nombre.length > 30) {
     Swal.fire("Nombre muy largo");
     return false;
@@ -32,16 +37,21 @@ if(Nombre.length > 30) {
     return false;
 }
 
-//Telefono
+if(Telefono.length >10 ) {
+    Swal.fire("Numero muy Largo");
+    return false;
+}
+if(Telefono.length <10 ) {
+    Swal.fire("Numero muy Corto");
+    return false;
+}
+
 if(isNaN(Telefono)){
-    Swal.fire("No es un nùmero")
+    Swal.fire("No es un numero")
     return false
 }
 
-if(!expresion1.test(Telefono)){
-    Swal.fire("No es un nùmero")
-    return false
-}
+
 
 
 
