d4bb9c14196eab6ba6046e5e961b0980ab368981
Juan Pablo <juanpismachado@gmail.com>
2020-12-08 11:13:22 -0500

    Prueba

--- AutoniteWeb-master 3/js/main.js
+++
@@ -1,7 +0,0 @@
-var btnSidebar = document.getElementById('sidebarCollapse'),
-    sidebar = document.getElementById('sidebar'),
-    content = document.getElementById('content');
-btnSidebar.addEventListener('click', function () {
-    sidebar.classList.toggle('hidden');
-    content.classList.toggle('hidden');
-});
